3f96c05eab7265fab8c44c9164b13fca840ed8ef
unluckypizzu <emiliopizzuto99@gmail.com>
2023-07-26 17:29:53 +0200

    Jasper report implementation

--- src/main/java/com/pizzu/graphpdf/controller/BookResolver.java
+++ src/main/java/com/pizzu/graphpdf/controller/BookResolver.java
@@ -4,6 +4,8 @@ import com.pizzu.graphpdf.entity.Author;
 import com.pizzu.graphpdf.entity.Book;
 import com.pizzu.graphpdf.repository.AuthorRepository;
 import com.pizzu.graphpdf.repository.BookRepository;
+import com.pizzu.graphpdf.service.ReportService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -11,6 +13,7 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import javax.transaction.Transactional;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,10 +23,13 @@ public class BookResolver {
     private BookRepository bookrepository;
     private AuthorRepository authorRepository;
 
+    private ReportService service;
+
     @Autowired
-    public BookResolver(BookRepository bookrepository, AuthorRepository authorRepository) {
+    public BookResolver(BookRepository bookrepository, AuthorRepository authorRepository, ReportService service) {
         this.bookrepository = bookrepository;
         this.authorRepository = authorRepository;
+        this.service = service;
     }
 
     @QueryMapping
@@ -43,6 +49,11 @@ public class BookResolver {
     public List<Book> getGenreBook(@Argument String genre) {
         List<Book> result = bookrepository.getBooksByGenre(genre);
         return result;
+    }
+
+    @QueryMapping
+    public String generateReport(@Argument String format) throws JRException, FileNotFoundException {
+        return service.exportReport(format);
     }
 
     @Transactional
---
+++ src/main/java/com/pizzu/graphpdf/service/ReportService.java
@@ -0,0 +1,51 @@
+package com.pizzu.graphpdf.service;
+
+
+import com.pizzu.graphpdf.entity.Book;
+import com.pizzu.graphpdf.repository.BookRepository;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private BookRepository repository;
+
+
+    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+
+        String path = "C:\\Users\\Pizzuto\\Desktop\\Exercise Training\\Report";
+
+        List<Book> bookList = repository.findAll();
+
+        File file = ResourceUtils.getFile("classpath:library.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(bookList);
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy","Pizzu");
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\library.html");
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\library.pdf");
+        }
+
+        return "Created in path " + path;
+    }
+}
